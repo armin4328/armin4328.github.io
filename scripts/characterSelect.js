5626cb447382e49b10344b6b4b1ebd9653c5255f
armin4328 <168665082+armin4328@users.noreply.github.com>
2024-09-10 14:12:17 -0500

    Add files via upload

---
+++ scripts/characterSelect.js
@@ -0,0 +1,28 @@
+function crusaderCharacterSelect() {
+    document.getElementById("crusader").style.display = "block";
+    document.getElementById("player1Choices").style.display = "none";
+    document.getElementById("turnsOutput").style.display = "block";
+
+    player1 = "crusader";
+}
+function houndmasterCharacterSelect() {
+    document.getElementById("houndmaster").style.display = "block";
+    document.getElementById("player1Choices").style.display = "none";
+    document.getElementById("turnsOutput").style.display = "block";
+
+    player1 = "houndmaster";
+}
+function bountyHunterCharacterSelect() {
+    document.getElementById("bountyHunter").style.display = "block";
+    document.getElementById("player2Choices").style.display = "none";
+    document.getElementById("turnsOutput").style.display = "block";
+
+    player2 = "bountyhunter";
+}
+function highwaymanCharacterSelect() {
+    document.getElementById("highwayman").style.display = "block";
+    document.getElementById("player2Choices").style.display = "none";
+    document.getElementById("turnsOutput").style.display = "block";
+
+    player2 = "highwayman";
+}
